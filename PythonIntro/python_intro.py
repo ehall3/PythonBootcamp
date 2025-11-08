29db5561f9f1c24ff8fae25c73cb1524f3777107
ehall3 <ellisonhall55@gmail.com>
2025-11-08 07:14:33 -0700

    Finished Python Intro Problems

--- PythonIntro/python_intro.py
+++ PythonIntro/python_intro.py
@@ -1,29 +1,37 @@
 # python_intro.py
 """Python Essentials: Introduction to Python.
-<Name>
-<Class>
-<Date>
+Ellison Hall
+Math Clinic
+November 8, 2025
 """
 
 
 # Problem 1 (write code below)
-
+if __name__ == "__main__":
+    print("Hello, world!")
 
 # Problem 2
 def sphere_volume(r):
     """ Return the volume of the sphere of radius 'r'.
     Use 3.14159 for pi in your computation.
     """
+    volume = (4/3)*(3.1415)*(r**3)
+    return volume
     raise NotImplementedError("Problem 2 Incomplete")
 
+if __name__ == "__main__":
+    print(sphere_volume(2))
 
 # Problem 3
 def isolate(a, b, c, d, e):
     """ Print the arguments separated by spaces, but print 5 spaces on either
     side of b.
     """
+    return (a,"     " + str(b) + "     ", c, d, e)
     raise NotImplementedError("Problem 3 Incomplete")
 
+if __name__ == "__main__":
+    print(isolate(1, 2, 3, 4, 5))
 
 # Problem 4
 def first_half(my_string):
@@ -36,7 +44,14 @@ def first_half(my_string):
         >>> first_half("ipython")
         'ipy'
     """
+
+    l = len(my_string)
+    l = l//2
+    return my_string[:l]
     raise NotImplementedError("Problem 4 Incomplete")
+
+if __name__ == "__main__":
+    print(first_half("python"))
 
 def backward(my_string):
     """ Return the reverse of the string 'my_string'.
@@ -47,8 +62,12 @@ def backward(my_string):
         >>> backward("ipython")
         'nohtypi'
     """
+    l = len(my_string)
+    return my_string[::-1]
     raise NotImplementedError("Problem 4 Incomplete")
 
+if __name__ == "__main__":
+    print(backward("python"))
 
 # Problem 5
 def list_ops():
@@ -66,8 +85,20 @@ def list_ops():
         >>> list_ops()
         ['fox', 'hawk', 'dog', 'bearhunter']
     """
+
+    animals = ["bear", "ant", "cat", "dog"]
+    animals.append("eagle")
+    animals[2] = "fox"
+    animals.pop(1)
+    animals.sort(reverse=True)
+    eagle_index = animals.index("eagle")
+    animals[eagle_index] = "hawk"
+    animals[-1] = animals[-1] + "hunter"
+    return animals
     raise NotImplementedError("Problem 5 Incomplete")
 
+if __name__ == "__main__":
+    print(list_ops())
 
 # Problem 6
 def pig_latin(word):
@@ -79,19 +110,45 @@ def pig_latin(word):
         >>> pig_latin("banana")
         'ananabay'
     """
+
+    vowels = "aeiou"
+    if word[0].lower() in vowels:
+        return word + "hay"
+    else:
+        return word[1:] + word[0] + "ay"
     raise NotImplementedError("Problem 6 Incomplete")
 
+if __name__ == "__main__":
+    print(pig_latin("apple"))
 
 # Problem 7
 def palindrome():
     """ Find and retun the largest panindromic number made from the product
     of two 3-digit numbers.
     """
+    largest = 0
+    for i in range(100, 1000):
+        for j in range(100, 1000):
+            product = i * j
+            if str(product) == str(product)[::-1]:  # check palindrome
+                if product > largest:
+                    largest = product
+    return largest
     raise NotImplementedError("Problem 7 Incomplete")
+
+if __name__ == "__main__":
+    print(palindrome())
 
 # Problem 8
 def alt_harmonic(n):
     """ Return the partial sum of the first n terms of the alternating
     harmonic series, which approximates ln(2).
     """
+    total = 0
+    for k in range(1, n + 1):
+        total += ((-1) ** (k + 1)) * (1 / k)
+    return total
     raise NotImplementedError("Problem 8 Incomplete")
+
+if __name__ == "__main__":
+    print(alt_harmonic(1))
